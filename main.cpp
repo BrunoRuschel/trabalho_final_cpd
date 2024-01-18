196ae060fc629bba047166e987c926db0dc7c223
BrunoRuschel <120383354+BrunoRuschel@users.noreply.github.com>
2024-01-18 16:54:56 -0300

    adicionando a estrutura 2.3
    aproveitei a função preenche_ratings, que já abre o arquivo dos ratings, para fazer a tabela hash dos usuários

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <iostream>
 #include <vector>
 #include <string>
 #include <fstream>
 #include <sstream>
 
 #define TAM 7993
+#define TAM_TB_USER 10007 //19999979
 
 using namespace std;
 
@@ -21,9 +21,45 @@ typedef struct {
     float count_ratings = 0.0;
 }JOGADOR;
 
+typedef struct {
+    int id;
+    vector<int> jogadores_avaliados;
+    vector<float> notas;
+}USER;
+
 int funcaoHash(int chave, vector<vector<JOGADOR>> &tb)
 {
     return chave % int(tb.size());
+}
+
+int funcaoHash_users(int chave, vector<vector<USER>> &tb_user)
+{
+    return chave % TAM_TB_USER;
+}
+
+void insere_user(vector<vector<USER>> &tb_user, USER &usuario)
+{
+    int id;
+    bool achou = false;
+    id = funcaoHash_users(usuario.id, tb_user);
+
+    //se o user ja esta na tabela, apenas adiciona o jogador e a nota
+    for (USER &user : tb_user[id]) {
+        if (user.id == usuario.id) {
+            user.jogadores_avaliados.insert(user.jogadores_avaliados.end(),
+                                             usuario.jogadores_avaliados.begin(),
+                                             usuario.jogadores_avaliados.end());
+            user.notas.insert(user.notas.end(),
+                                             usuario.notas.begin(),
+                                             usuario.notas.end());
+            achou = true;
+        }
+    }
+
+    //se nao esta na tabela, adiciona esse usuario
+    if(achou == false) {
+        tb_user[id].push_back(usuario);
+    }
 }
 
 void insere(vector<vector<JOGADOR>> &tb, JOGADOR jogador)
@@ -96,8 +132,7 @@ void lerArquivo_fillTabela(vector<vector<JOGADOR>> &tb, const string &nomeArquiv
     }
 }
 
-void preenche_ratings(vector<vector<JOGADOR>> &tb, const string &nomeArquivo) {
-    vector<JOGADOR> jogadores;
+void preenche_ratings(vector<vector<JOGADOR>> &tb, const string &nomeArquivo, vector<vector<USER>> &tb_user) {
     ifstream arquivo(nomeArquivo);
     int sofifa_id;
     float nota;
@@ -114,17 +149,21 @@ void preenche_ratings(vector<vector<JOGADOR>> &tb, const string &nomeArquivo) {
             string token;
 
             JOGADOR jogador;
+            USER usuario;
 
             // Read user_id
             getline(ss, token, ',');
+            usuario.id = stoi(token); //2.3
 
             // Read sofifa_id
             getline(ss, token, ',');
             sofifa_id = stoi(token);
+            usuario.jogadores_avaliados.push_back(sofifa_id); //2.3
 
             // Read rating
             getline(ss, token, ',');
             nota = stof(token);
+            usuario.notas.push_back(nota); //2.3
 
             int hashValue = funcaoHash(sofifa_id, tb);
 
@@ -132,9 +171,10 @@ void preenche_ratings(vector<vector<JOGADOR>> &tb, const string &nomeArquivo) {
                 if (jogador.id == sofifa_id) {
                     jogador.soma_ratings += nota;
                     jogador.count_ratings++;
-                    break; // Once updated, exit the loop
+                    break;
                 }
             }
+            insere_user(tb_user, usuario); //2.3
         }
 
         arquivo.close();
@@ -160,18 +200,42 @@ JOGADOR busca(vector<vector<JOGADOR>> &tb, int chave) {
     return jog_nulo;
 }
 
+USER busca_user(vector<vector<USER>> &tb_user, int chave) {
+    int id;
+    USER user_nulo;
+
+    user_nulo.id = 0;
+    user_nulo.jogadores_avaliados.push_back(0);
+    user_nulo.notas.push_back(0.0);
+
+    id = funcaoHash_users(chave, tb_user);
+
+    for (const USER &usuario : tb_user[id]) {
+        if (usuario.id == chave) {
+            return usuario;
+        }
+    }
+
+    return user_nulo;
+}
+
 int main()
 {
     vector<vector<JOGADOR>> tb(TAM, vector<JOGADOR>());
+    vector<vector<USER>> tb_user(TAM_TB_USER, vector<USER>());
 
     lerArquivo_fillTabela(tb, "players.csv");
-    preenche_ratings(tb, "minirating.csv");
+    preenche_ratings(tb, "minirating.csv", tb_user);
 
-    JOGADOR jog;
+    //JOGADOR jog;
+    //USER user;
 
-    jog = busca(tb, 191043);
+    //jog = busca(tb, 191043);
+    //user = busca_user(tb_user, 21872);
 
-    cout << jog.soma_ratings / jog.count_ratings << endl;
+    //cout << jog.soma_ratings / jog.count_ratings << endl;
+    //cout << user.jogadores_avaliados[0] << " " << user.notas[0] << endl;
+    //cout << user.jogadores_avaliados[1] << " " << user.notas[1] << endl;
 
     return 0;
 }
